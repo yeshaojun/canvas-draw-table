4faaccbf7d9c458600ceebe829534ddcb7840107
yeshaojun <yeshaojun5056@163.com>
2022-06-18 06:39:48 +0800

    fix: config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,16 @@
 const path = require("path");
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/plugin/index.js",
   mode: "production",
   output: {
-    filename: "[name].bundle.js",
+    filename: "canvas-draw-table.js",
     path: path.resolve(__dirname, "lib"),
     library: "$DrawTable",
-    libraryTarget: "umd",
+    libraryTarget: 'umd'
   },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
-    },
-    compress: true,
-    port: 9000,
-    hot: true,
-  },
-  plugins: [new CleanWebpackPlugin(), new UglifyJsPlugin({
-    test: /\.js($|\?)/i
-  })],
+  plugins: [new CleanWebpackPlugin()],
   module: {
     rules: [
       {
