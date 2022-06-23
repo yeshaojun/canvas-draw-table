36bb963fe1daea38739bff06ecaad14f514bcc81
yeshaojun <sjye@rsvptech.cn>
2022-06-23 15:38:46 +0800

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,12 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = {
   entry: "./src/plugin/index.js",
-<<<<<<< HEAD
-  mode: "development",
-=======
   mode: "production",
->>>>>>> 497e9084ceeabedfd184c1fd50d2eb6764da8324
   output: {
     filename: "canvas-draw-table.js",
     path: path.resolve(__dirname, "lib"),
     library: "$DrawTable",
-    libraryTarget: 'umd'
+    libraryTarget: "umd",
   },
   plugins: [
     new CleanWebpackPlugin(),
